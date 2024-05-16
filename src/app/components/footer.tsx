a50f09aa6e773eb8695f4fe59394ad3b5a4ee02a
spicychillisus <asherben90@gmail.com>
2024-05-16 10:15:06 +0800

    footer text styling

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <footer>
-            <h3 className="text-bold"></h3>
+        <footer className="bg-gray-800">
+            <h3 className="text-bold absolute bottom-0 w-full text-center">test</h3>
         </footer>
     )
 }
