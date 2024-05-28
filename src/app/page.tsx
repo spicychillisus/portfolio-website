453e21c9e7fae321f90091ea4003ff61dae423f6
spicychillisus <asherben90@gmail.com>
2024-05-29 01:34:50 +0800

    yes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
         <div className="grid md:grid-cols-2 gap-10">
             <div className="bg-gray-800 rounded-xl pl-2 pt-2">
               <h1 className="uppercase text-2xl font-bold">Introduction</h1>
-              <p className="">
+              <p className="font-normal">
                 Hello! My name is Asher, and I'm currently pursuing a Diploma in Information Technology in Singapore Polytechnic,
                 specialising in Software Development.</p>
             </div>
-            <div className="bg-gray-800 rounded-md">
+            <div className="bg-gray-800 rounded-xl">
               test
             </div>
         </div>
