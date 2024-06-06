b503dea033ee4e657be9471f6eb66edb109e0ee9
spicychillisus <asherben90@gmail.com>
2024-06-06 10:46:33 +0800

    hi

---
+++ src/app/components/navbar/button.tsx
@@ -0,0 +1,5 @@
+// this button will only be displayed when viewed on mobile phone
+
+const ButtonMenu = () => {
+    
+}
--- src/app/resume/experience.tsx
+++ src/app/resume/experience.tsx
@@ -1,6 +1,34 @@
 
-const Experience = () => {
+const experiences = [
+    {
+        id: 1,
+        tittle: "Portfolio Website",
+        description: "You are looking at it now! This website was made with TailwindCSS and Nextjs 14.",
+        link: ""
+    },
+    {
+        id: 2,
+        tittle: "Portfolio Website",
+        description: "You are looking at it now! This website was made with TailwindCSS and Nextjs 14."
+    },
+    {
+        id: 3,
+        tittle: "Portfolio Website",
+        description: "You are looking at it now! This website was made with TailwindCSS and Nextjs 14."
+    },
+    {
+        id: 4,
+        tittle: "Portfolio Website",
+        description: "You are looking at it now! This website was made with TailwindCSS and Nextjs 14."
+    }
+]
 
+
+
+function Experience() {
+    const display = experiences.forEach((e) => {
+        
+    })
 }
 
 export default Experience;
